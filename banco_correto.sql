5b396af2217ef9ade5a6e6a04ad6665f3e94cd1d
italo2403 <italonunespereira@gmail.com>
2024-10-25 12:36:08 -0300

    Projeto Correto

---
+++ banco_correto.sql
@@ -0,0 +1,52 @@
+create database teste;
+
+use teste;
+
+CREATE TABLE imagens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_arquivo VARCHAR(255) NOT NULL,
+    caminho VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE artista (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    area VARCHAR(100) NOT NULL,
+    estilo VARCHAR(100) NOT NULL
+   
+);
+
+
+insert into usuario_comun(nome, email,senha) values('daniela','daniela@daniela', '123');
+create table usuario_comun(
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+create table empresa(
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL UNIQUE,
+senha VARCHAR(255) NOT NULL,
+cnpj varchar(30),
+nome_empresa VARCHAR(100) NOT NULL,
+fone varchar(20)
+);
+
+
+
+
+
+select * from artista;
+drop table artista;
